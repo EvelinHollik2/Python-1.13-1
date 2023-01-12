6e318a42013ce1ee120b7bc67960494f0966c7f6
EvelinHollik2 <hollikevi@gmail.com>
2023-01-12 10:41:33 +0100

    fgd

---
+++ 7.26/14.py
@@ -0,0 +1,4 @@
+def szamjegy_szam(sz):
+    
+    print(szamjegy_szam(0))
+    print(szamjegy_szam(-12345))
---
+++ 7.26/15.py
@@ -0,0 +1,14 @@
+def paros_szamjegy_szam(n):
+    szamlalo = 0
+    while n >= 0:
+        szamlalo = szamlalo +1
+        n = n //10
+        return szamlalo
+for i in range:
+    print(paros_szamjegy_szam(123456))
+    print(paros_szamjegy_szam(2468))
+    print(paros_szamjegy_szam(1357))
+    print(paros_szamjegy_szam(0))
+    if i % 2 ==1 :
+        continue
+    print(i)
